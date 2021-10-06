e4c468522aa617b167f42c035a5ae6b341d0be15
Bott006 <92000576+Bott006@users.noreply.github.com>
2021-10-06 00:04:09 -0500

    Add files via upload

---
+++ RutaArtesanal.Application/MessageService.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.RegularExpressions;
+using System;
+using RutaArtesanal.Infrastructure;
+
+namespace RutaArtesanal.Application
+{
+    public class MessageService
+    {
+        public string SayHello(string name){
+            var repo = new MessageRepository();
+            var msg = repo.GetFirst();
+            return $"{msg.Summary} {name}";
+        }
+    }
+}
