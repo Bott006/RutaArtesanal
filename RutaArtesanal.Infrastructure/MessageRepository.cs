548f9368a846060dd2614762720e45d298d6021c
Bott006 <92000576+Bott006@users.noreply.github.com>
2021-10-06 00:06:17 -0500

    Add files via upload

---
+++ RutaArtesanal.Infrastructure/MessageRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using RutaArtesanal.Domain;
+
+namespace RutaArtesanal.Infrastructure
+{
+    public class MessageRepository
+    {
+        public Message GetFirst(){
+            return new Message("Jeloudah");
+        }
+        public Message2 GetSecond(){
+            return new Message2("Segundo mensaje.e");
+        }
+    }
+}
